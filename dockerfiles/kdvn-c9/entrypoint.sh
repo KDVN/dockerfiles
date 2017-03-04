a4e2fc36709af81b3f3a6336a8fde3377ff99d0d
dnt83 <duongcdtin5@gmail.com>
2017-03-04 16:38:02 +0700

    Update

--- dockerfiles/kdvn-c9/entrypoint.sh
+++ dockerfiles/kdvn-c9/entrypoint.sh
@@ -7,7 +7,7 @@ fi
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
 
-USER_ID=${LOCAL_USER_ID}
+USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID:-9001"
 sudo usermod -u $USER_ID devuser
