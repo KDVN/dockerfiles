99ffb3daeb46075726ffecba328bf8f3a6540862
dnt83 <duongcdtin5@gmail.com>
2017-04-20 09:23:31 +0000

    Update

--- dockerfiles/kdvn-odoo8-c9/entrypoint.sh
+++ dockerfiles/kdvn-odoo8-c9/entrypoint.sh
@@ -12,6 +12,6 @@ USER_ID=${LOCAL_USER_ID}
 echo "Starting with UID : $USER_ID:-9001"
 sudo usermod -u $USER_ID odoo
 
-exec nodejs server.js $AUTH --port 3000 -w /opt/workspace/
+bash -c 'nodejs server.js $AUTH --port 3000 -w /opt/workspace/; /bin/bash'
 
 exit 1
--- dockerfiles/kdvn-oe-c9/entrypoint.sh
+++ dockerfiles/kdvn-oe-c9/entrypoint.sh
@@ -12,6 +12,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID:-9001"
 sudo usermod -u $USER_ID devuser
 
-exec nodejs server.js $AUTH --port 3000 -w /opt/workspace/
+bash -c 'nodejs server.js $AUTH --port 3000 -w /opt/workspace/; /bin/bash'
+#exec nodejs server.js $AUTH --port 3000 -w /opt/workspace/
 
 exit 1
